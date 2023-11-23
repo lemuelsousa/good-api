5a4d8d3f0e06613d9df803f0313d2df40fe5e070
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-11-23 09:53:35 -0300

    feat: create Auth Exception

---
+++ app/src/main/java/good/api/infrastructure/services/auth/exception/AuthException.java
@@ -0,0 +1,9 @@
+package good.api.infrastructure.services.auth.exception;
+
+public class AuthException extends RuntimeException{
+
+    public AuthException(String message) {
+        super(message);
+    }
+
+}
