dda99088354f8a17eb9bb68fd3d9d9592256fc86
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-11-24 10:55:09 -0300

    refact: add refresh token and access token

--- app/src/main/java/good/api/web/controllers/auth/dto/LoginResponse.java
+++ app/src/main/java/good/api/web/controllers/auth/dto/LoginResponse.java
@@ -1,6 +1,10 @@
 package good.api.web.controllers.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record LoginResponse(
-        String token
+        @JsonProperty("access_token") String accessToken,
+        @JsonProperty("refresh_token") String refreshToken
+
 ) {
 }
